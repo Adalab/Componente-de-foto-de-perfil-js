b34ac347325517b802236f820bc0d481e3a84868
Carlos Mañas <carlos@spacenomads.com>
2019-02-06 11:32:31 +0100

    Add avatar component

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Componente-de-foto-de-perfil
+# Componente de foto de perfil
 Componente listo para usar con la subida de imagen de perfil
+
+## Elementos
+Para que funcione correctamente necesitaremos estos elementos:
+- **Campo de tipo "file"**: no podemos darles estilos correctamente pero lo necesitamos para añadir un archivo; lo vamos a ocultar ;)
+- **Botón**: Podemos darle los estilos que queramos y al hacer click en él generaremos un clic automático en el campo oculto
+- **Contenedor principal de la imagen**: tiene una imagen por defecto a través del atributo style
+- **Contenedor de la previsualización de la imagen**: por defecto aparece vacío
+
+## Cómo usarlo
+Tendremos que enlazar el archivo `get-avatar.js` a nuestro html y asegurarnos de que nuestros cuatro elementos tienen estas clases (además de las que usemos para dar estilos):
+
+| Elemento | Clase |
+|---|---|
+| Campo de tipo "file" | `js__profile-upload-btn` |
+| Botón | `js__profile-trigger`|
+| Contenedor principal de la imagen | `js__profile-image` |
+| Contenedor de la previsualización de la imagen | `js__profile-preview` |
+
+>  Nota de estilos:  
+La hoja de estilos del proyecto es para verlo funcionar. Lo único a tener en cuenta es que el campo de tipo "file" debe ocultarse.
+
+***
+
+** Al turrón!**
---
+++ js/get-avatar.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fr = new FileReader();
+const uploadBtn = document.querySelector('.js__profile-trigger');
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
+
+
+/**
+ * Recoge el archivo añadido al campo de tipo "file"
+ * y lo carga en nuestro objeto FileReader para que 
+ * lo convierta a algo con lo que podamos trabajar.
+ * Añade un listener al FR para que ejecute una función
+ * al tener los datos listos
+ * @param {evento} e 
+ */
+function getImage(e){
+  var myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+}
+
+
+/**
+ * Una vez tenemos los datos listos en el FR podemos
+ * trabajar con ellos ;)
+ */
+function writeImage() {
+  /* En la propiedad `result` de nuestro FR se almacena
+   * el resultado
+   */
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+}
+
+
+/**
+ * Genera un click automático en nuesto campo de tipo "file"
+ * que está oculto
+ */
+function fakeFileClick() {
+ fileField.click(); 
+}
+
+/**
+ * Añadimos los listeners necesarios:
+ * - al botón visible para generar el click automático
+ * - al campo oculto para cuando cambie su value
+ */
+uploadBtn.addEventListener('click', fakeFileClick);
+fileField.addEventListener('change', getImage);
